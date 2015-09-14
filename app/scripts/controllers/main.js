73e533812d22a98d14bb43a5e11c3e14d07575b7
臼井健一 <usustek@usuikenichi-no-MacBook-Pro.local>
2015-09-14 18:06:53 +0900

    adding parse functions, parse char display.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the rapicaAnalyzeApp
  */
 angular.module('rapicaAnalyzeApp')
-  .controller('MainCtrl', function ($scope, RapicaParse) {
+  .controller('MainCtrl', function ($scope, $window, RapicaParse) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -23,6 +23,23 @@ angular.module('rapicaAnalyzeApp')
       $scope.refreshData();
     };
 
+    $scope.onselect = function (name) {
+      var pt = $scope.parsedData.point;
+      if ((undefined !== pt) && (null !== pt)) {
+        if (pt[name] !== undefined) {
+          var minIdx = pt[name][0];
+          var maxIdx = pt[name][1];
+          angular.forEach(hexStrDisp.children, function (elm, index) {
+            if((minIdx <= index) && (index <= maxIdx)){
+              elm.style.color = "lime";
+            }else{
+              elm.style.color = "black";
+            }
+          });
+        }
+      }
+    };
+    
     $scope.refreshData = function(){
         // 強制的にデータを反映
         if(!$scope.$$phase){
--- app/scripts/services/rapicaparse.js
+++ app/scripts/services/rapicaparse.js
@@ -10,8 +10,6 @@
 angular.module('rapicaAnalyzeApp')
   .service('RapicaParse', function () {
     return {
-      isCity : false,
-      
       parse: function(hexStr){
         hexStr = hexStr.trim();
         
@@ -21,17 +19,30 @@ angular.module('rapicaAnalyzeApp')
             var dat = parseInt(hexStr.charAt(i+0)+hexStr.charAt(i+1), 16);
             hexs.push(dat);
           }
+          var res = { point: {} };
           
-          return {
-            date: this.parseDate([hexs[0], hexs[1], hexs[2]]),
-            corp: this.parseCorp(hexs[3])
-          };
+          this.parseHexStr(res, hexStr);
+          this.parseDate(res, [hexs[0], hexs[1], hexs[2]]);
+          this.parseCorp(res, hexs[3]);
+          this.parseBusGroup(res, hexs);
+          this.parseBusStop(res, hexs);
+          this.parseUsing(res, hexs);
+          this.parseRemain(res, hexs);
+          
+          return res;
         }
         
         return {};
       },
       
-      parseDate: function(hex3){
+      parseHexStr: function(res, hexStr){
+        res.hexStr = "";
+        hexStr.split('').forEach(function(element, index) {
+          res.hexStr += "<span id='char" + index.toString() + "'>" + element + "</span>";
+        }, this);
+      },
+      
+      parseDate: function(res, hex3){
         var md = (hex3[0] << 4) + (hex3[1] >>> 4);
         var hm = ((hex3[1] & 0x0f) << 8) + hex3[2];
         
@@ -40,31 +51,101 @@ angular.module('rapicaAnalyzeApp')
         var min  = hm % 100;
         var hour =  (hm - min) / 100;
         
-        return new Date(new Date().getFullYear(), mon -1, day, hour, min, 0);
+        res.point["date"] = [0,5];
+        res.date = new Date(new Date().getFullYear(), mon -1, day, hour, min, 0);
       },
       
-      parseCorp : function(hex1) {
+      parseCorp : function(res, hex1) {
         var corp = hex1 >>> 4;
 
-        this.isCity = false;
+        res.isCity = false;
+        res.point["corp"] = [6,7];
                 
         switch(corp){
           case 1:
-            this.isCity = true;
-            return "鹿児島市";
+            res.isCity = true;
+            res.corp = "鹿児島市";
+            break;
           case 2:
-            this.isCity = true;
-            return "南国交通";
-          case 3: return "JR九州";
-          case 4: return "いわさき";
-          case 5: return "林田";
+            res.isCity = true;
+            res.corp = "南国交通";
+            break;
+          case 3: 
+            res.isCity = true;
+            res.corp = "JR九州";
+            break;
+          case 4:
+            res.corp = "いわさき";
+            break;
+          case 5:
+            res.corp = "林田";
+            break;
           default:
-            return "";
+            res.corp = "";
+            break;
         }
       },
       
-      parseGroup : function(hexs) {
+      parseBusGroup : function(res, hexs) {
+        var val = null;
+        if(res.isCity) {
+          val = (hexs[7] << 8 ) | hexs[8];
+          res.point["busGroup"] = [14,17];
+        }
+        else{
+          val = (hexs[7] << 16) | (hexs[8] << 8) | hexs[9];
+          res.point["busGroup"] = [14,19];
+        }
         
-      }
+        if(null !== val){
+          res.busGroup = "0x" + ("0000"+ val.toString(16)).substr(-4);
+        }
+      },
+      
+      parseBusStop : function(res, hexs){
+        var val = null;
+        
+        if(res.isCity){
+          val = (hexs[4] << 16) | (hexs[5] << 8) | hexs[6];
+          res.point["busStop"] = [8,13];
+        }
+        else{
+          val = (hexs[5] << 8) | hexs[6];
+          res.point["busStop"] = [10,13];
+        }
+        
+        if (val !== null){
+          res.busStop = "0x" + ("0000" + val.toString(16)).substr(-4);
+        }
+      },
+      
+      parseUsing : function(res, hexs){
+        res.point["using"] = [24,25];
+        switch(hexs[12]){
+          case 0x00:
+            res.using = "作成";
+            break;
+          case 0x10:
+            res.using = "登録";
+            break;
+          case 0x20:
+            res.using = "積増";
+            break;
+          case 0x30:
+            res.using = "乗車";
+            break;
+          case 0x41:
+            res.using = "降車";
+            break;
+          case 0x44:
+            res.using = "降車(割)";
+            break;
+        }
+      },
+      
+      parseRemain : function(res, hexs){
+        res.point["remain"] = [26,31];
+        res.remain = (hexs[13] << 16) | (hexs[14] << 8) | hexs[15];
+      },
     };
   });
